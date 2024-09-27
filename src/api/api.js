040798111e0e67c67c267ccd64064fac4d46cb8c
Huuwi <110718832+Huuwi@users.noreply.github.com>
2024-09-27 13:35:22 +0700

    change

---
+++ src/api/api.js
@@ -0,0 +1,47 @@
+const api = require("express").Router()
+const Controler = require("../controler/controler.js")
+const MiddleWare = require("../middleware/middleware.js")
+
+//create a instance
+let controler = new Controler()
+let middleware = new MiddleWare()
+
+//test api
+api.get("/", (req, res) => {
+    res.cookie("test", "test", {
+        httpOnly: true
+    });
+
+    res.send("hello from backend");
+})
+
+//use middleware
+
+//check ip black list
+api.use("*", middleware.checkIpAdress)
+
+
+
+// middle ware for /auth api
+api.use("/auth", middleware.checkInforAccessToken)
+
+
+
+
+//api login
+api.post("/login", controler.loginAccount)
+
+
+//api register
+api.post("/register", controler.registerAccount)
+
+//get new access token
+api.post("/getNewAccessToken", controler.getNewAccessToken)
+
+
+//auth_api
+api.get("/auth/getInforUser", controler.getInforUser) // font end make request for data user
+api.post("/auth/loginHaui", controler.loginHaui) // login haui sv
+
+
+module.exports = api
---
+++ src/database/createTable.js
@@ -0,0 +1,77 @@
+require('dotenv').config({ path: '../.env' })
+const { connection } = require("./connection.js")
+
+const main = async () => {
+
+    await connection.connect()
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+
+
+
+    // await connection.excuteQuery("create table user (userId int NOT NULL AUTO_INCREMENT PRIMARY KEY, userName varchar(20) NOT NULL unique, passWord varchar(100) NOT NULL, referralCode varchar(70) NOT NULL, balance double default 0 , timeCreate varchar(30) default 'none' )")
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+    //     .catch((e) => {
+    //         console.log(e);
+    //     })    //done
+
+    // await connection.excuteQuery("alter table user modify column passWord varchar(100) default 'none' ")
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+    //     .catch((e) => {
+    //         console.log(e);
+    //     }) //done
+
+
+    // await connection.excuteQuery(
+    //     `UPDATE user
+    //     SET balance = 999999, isAdmin = 1
+    //     WHERE userId = 13;`
+    // )
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+    //     .catch((e) => {
+    //         console.log(e);
+    //     }) // done
+
+
+    // for (let i = 10; i <= 12; i++) {
+    //     await connection.excuteQuery(`delete from user where userId = ${i}`)
+    //         .then((res) => {
+    //             console.log(res);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         })
+    // }
+
+
+
+
+    await connection.excuteQuery("select * from user")
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+
+
+    await connection.disconnect()
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+
+}
+main()
