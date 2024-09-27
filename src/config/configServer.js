c7c3c4be63704e1f5566d377ac982f3428baeedd
Huuwi <110718832+Huuwi@users.noreply.github.com>
2024-09-27 17:49:43 +0700

    change

--- src/config/configServer.js
+++ src/config/configServer.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser')
 
 function configServer(app) {
     app.use(cors());
+    app.use(cors({
+        origin: '*', // Cho phép tất cả các nguồn
+    }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }))
     app.use(cookieParser())
