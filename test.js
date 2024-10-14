39cda2b5cfe8e06e343934bf8a73c54a37d448f6
Huuwi <110718832+Huuwi@users.noreply.github.com>
2024-10-14 22:00:05 +0700

    module_haui

---
+++ test.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+let patternNameModule = /(?<module_name>^.+?)\s--\s(?<module_id>\d+)\s+module_code\s:\s(?<module_code>.+)/
+
+let oldData = fs.readFileSync("./module.txt", 'utf-8').split("\r\n");
+
+let res = []
+
+// for (let i = 0; i < oldData.length; i++) {
+//     let result = oldData[i].match(patternNameModule);
+//     // console.log(result);
+
+//     if (result) {
+//         res.push(result.groups)
+//     }
+// }
+
+let test = "thực tập cơ sở ngành á ắ ớ bú buồi"
+console.log(test.normalize("NFKD").replace(/[\u0300-\u036f]/g, ''));
